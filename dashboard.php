caca9e76f73f214d1fdf41297bdf28e0df4befc1
Malek F <43182938+MalekFarag@users.noreply.github.com>
2020-02-06 15:47:39 -0500

    date/time more user friendly + last login fixed

--- dashboard.php
+++ dashboard.php
@@ -10,14 +10,22 @@
     <h2>Welcome admin</h2>
 
     <?php
+        //timezone config
+        date_default_timezone_set('America/Toronto');
+
+
+        //connecting to db
         $conn = mysqli_connect('localhost', 'root', '','db_admin_research');
+        //selecting user_date (last login)
         $sql_date = mysqli_query($conn, 'SELECT user_date FROM tbl_user');
-
+        //formating date for user 
         $res_date = mysqli_fetch_row($sql_date);
+        $last_login = date('d F Y H:i', strtotime($res_date[0]));
+        //displaying last login date on page
+        echo '<p>Last time logged in: '.$last_login.'</p>';
 
-        echo '<p>Last time logged in: '.$res_date[0].'</p>';
 
-        // welcome messages
+        // welcome messages and current time
         $hour = date('H');
         $currentTime = date("h:i a");
 
@@ -31,7 +39,12 @@
             echo '<h3> good evening </h3>
             <p>the current time is: '.$currentTime.'</p>';
         }
+        
 
+        // update user_date
+        $currentDate = date('Y-m-d H:i:s');
+        mysqli_query($conn, "UPDATE tbl_user SET user_date='".$currentDate."'");
+        exit;
 
 
     ?>
